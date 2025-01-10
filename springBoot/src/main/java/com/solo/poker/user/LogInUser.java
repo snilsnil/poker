3cce7f6321b77763535f7a7bca3ea6e2e658e09a
snilsnil <heo6601@naver.com>
2025-01-10 23:47:49 +0900

    remove entity option (LogInUser.java)

--- springBoot/src/main/java/com/solo/poker/user/LogInUser.java
+++ springBoot/src/main/java/com/solo/poker/user/LogInUser.java
@@ -1,14 +1,12 @@
 package com.solo.poker.user;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
 public class LogInUser {
 
     @Id
